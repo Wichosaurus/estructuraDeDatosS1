f21a9a35984da44e77ee5612517ea7ce71e1e2a8
Luis Hernandez <luishcampos92@gmail.com>
2024-06-02 11:37:14 -0600

    Probando enlazar terminal con GH en clase de Etrucutra de Datos

---
+++ 01-pilas.js
@@ -0,0 +1,18 @@
+class Carro{
+    let color = null;
+    let marca = null;
+    let modelo = null;
+    let precio = null;
+    let velocidad = null;
+
+    constructor(color, marca, modelo, precio, velocidad){
+
+    }
+    const Acelerar = () => {});
+    const Frenar = () => {});
+    const Girar = () => {});
+    const Encender = () => {});
+    const Apagar = () => {});
+    const cambiarVelocidad = () => {});
+}
+
